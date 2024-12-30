9eaa184cf80b3ba7d6528cf44569b917f74986f3
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-30 15:27:38 +0100

    Day 24, p2 WIP

--- src/AOC2024/Day24/Puzzle2.php
+++ src/AOC2024/Day24/Puzzle2.php
@@ -2,22 +2,26 @@
 
 namespace Jelle_S\AOC\AOC2024\Day24;
 
-use Jelle_S\AOC\Contracts\PuzzleInterface;
-
-class Puzzle1 implements PuzzleInterface {
-
-  public function __construct(protected string $input) {
-  }
+class Puzzle2 extends Puzzle1 {
 
   public function solve() {
     $result = 0;
 
     $input = trim(file_get_contents($this->input));
     list($valuesDescriptions, $gateDescriptions) = explode("\n\n", $input);
-    $values = [];
+    $xBin = $yBin = '';
+    $inputValues = [];
     foreach (explode("\n", $valuesDescriptions) as $v) {
       list($key, $value) = explode(': ', $v);
-      $values[$key] = intval($value);
+      $inputValues[$key] = intval($value);
+      switch(substr($key, 0, 1)) {
+        case 'x':
+          $xBin .= $value;
+          break;
+        case 'y':
+          $yBin .= $value;
+          break;
+      }
     }
 
     $gates = [];
@@ -27,12 +31,34 @@ class Puzzle1 implements PuzzleInterface {
       $gates[$output] = [$in1, $in2, $operator, $output];
     }
 
-    $values = array_filter($this->resolveKahns($gates, $values), fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
+    for ($i = 0; $i < 44; $i++) {
+      $inputValues['x' . str_pad($i, 2, '0', STR_PAD_LEFT)] = 0;
+      $inputValues['y' . str_pad($i, 2, '0', STR_PAD_LEFT)] = 1;
+    }
+    list($values, $paths) = $this->resolveKahns($gates, $inputValues);
+    $values = array_filter($values, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
     krsort($values);
-    return bindec(implode('', array_map('strval', $values)));
+    $endgates = array_keys($values);
+
+    $real = implode('', array_map('strval', $values));
+    $expected = bindec($xBin) + bindec($yBin);
+    $mismatch = bindec($real) ^ $expected;
+
+    $pairUsage = new \Ds\Set();
+    foreach ($this->bitPositionsInMask($mismatch) as $position) {
+      $pairUsage = $pairUsage->merge($paths[$endgates[$position]]);
+    }
+
+    arsort($pairUsage);
+    foreach ($pairUsage as $pair) {
+      
+    }
+
+    return count($pairUsage);
   }
 
   protected function resolveKahns(array $gates, array $values) {
+    $valuePaths = [];
     $indegrees = array_fill_keys(array_keys($gates), 0);
     // Build a graph where we map all inputs to the operations they're part of.
     $graph = [];
@@ -46,8 +72,9 @@ class Puzzle1 implements PuzzleInterface {
     }
 
     $q = new \Ds\Queue();
-    foreach (array_keys($values) as $gate) {
+    foreach ($values as $gate => $value) {
       $q->push($gate);
+      $valuePaths[$gate] = new \Ds\Set();
     }
 
     while(!$q->isEmpty()) {
@@ -65,6 +92,12 @@ class Puzzle1 implements PuzzleInterface {
         // All the inputs of this gate are resolved, process it and add it to
         // the queue.
         if ($indegrees[$output] === 0) {
+
+          $valuePaths[$output] = $valuePaths[$current]->copy();
+          $pair = [$in1, $in2];
+          sort($pair);
+          $valuePaths[$output]->add($pair);
+
           switch ($operator) {
             case 'AND':
               $values[$output] = $values[$in1] & $values[$in2];
@@ -82,6 +115,27 @@ class Puzzle1 implements PuzzleInterface {
       }
     }
 
-    return $values;
+    return [$values, $valuePaths];
+  }
+
+  protected function bitPositionsInMask($mask) {
+    $positions = [];
+    $len = strlen(decbin($mask));
+    for($i = 0; $i < $len; $i++) {
+      $bit = 1 << $i;
+      if ($mask & $bit) {
+        $positions[] = $i;
+      }
+    }
+
+    return $positions;
+  }
+
+  protected function bitwiseNot($mask) {
+    $len = strlen(decbin($mask));
+    // "All 1" mask to fix PHP's weird "bitwise not" behavior.
+    $fixMask = (1 << $len) - 1;
+
+    return ~$mask & $fixMask;
   }
 }
