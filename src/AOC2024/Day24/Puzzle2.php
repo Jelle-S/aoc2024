84d8cdb74ca187f7c01c359161bd265dff7bdcb2
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-31 17:11:26 +0100

    Try, fail, repeat

--- src/AOC2024/Day24/Puzzle2.php
+++ src/AOC2024/Day24/Puzzle2.php
@@ -42,116 +42,27 @@ class Puzzle2 extends Puzzle1 {
     }
 
     $expected = bindec($xBin) + bindec($yBin);
-    $swapped = new \Ds\Set();
-    while (true) {
-      list($values, $paths) = $this->resolveKahns($gates, $inputValues);
-      $values = array_filter($values, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
-      //$paths = array_filter($paths, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
-      krsort($values);
-      krsort($paths);
-      var_dump($paths['z00'],$paths['z01'], $paths['njb'], $paths['tkb']); exit;
-      $wrongBit = $this->getLeastSignificantWrongBit($values, $expected);
-      if ($wrongBit === -1) {
-        break;
-      }
-      $validPairs = new \Ds\Set();
-      for ($i = 0; $i < $wrongBit; $i++) {
-        $validPairs = $validPairs->merge($paths['z' . str_pad($i, 2, '0', STR_PAD_LEFT)]);
-      }
-      $invalidPairs = $paths['z' . str_pad($wrongBit, 2, '0', STR_PAD_LEFT)];
-      foreach ($invalidPairs->diff($validPairs) as $invalidPair) {
-        $gate1 = $gatesByPair->get($invalidPair);
-        foreach ($gates as $gate2) {
-          if ($gate1 === $gate2) {
-            continue;
-          }
-          $oldgates = $gates;
-          $gates[$gate1[3]] = $gate2;
-          $gates[$gate1[3]][3] = $gate1[3];
-          $gates[$gate2[3]] = $gate1;
-          $gates[$gate2[3]][3] = $gate2[3];
-          list($newValues, $newPaths) = $this->resolveKahns($gates, $inputValues);
-          $newValues = array_filter($newValues, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
-          $newPaths = array_filter($newPaths, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
-          krsort($newValues);
-          $newWrongBit = $this->getLeastSignificantWrongBit($newValues, $expected);
-          if (($newWrongBit <= $wrongBit && $newWrongBit !== -1) || count($newValues) !== strlen(decbin($expected))) {
-            $gates = $oldgates;
-            continue;
-          }
-          $pair1 = [$gates[$gate1[3]][0], $gates[$gate1[3]][1]];
-          $pair2 = [$gates[$gate2[3]][0], $gates[$gate2[3]][1]];
-          $swapped->add($gate1[3]);
-          $swapped->add($gate2[3]);
-          sort($pair1);
-          sort($pair2);
-          $gatesByPair->put($pair1, $gate1);
-          $gatesByPair->put($pair2, $gate2);
-          continue 2;
-        }
-      }
-    }
-    $swapped->sort();
-    return implode(',', $swapped->toArray());
+
+    $values = $this->resolveKahns($gates, $inputValues);
+    $values = array_filter($values, fn($k) => substr($k, 0, 1) === 'z', ARRAY_FILTER_USE_KEY);
+    krsort($values);
+    $debugPaths = array_filter($paths, fn($k) => in_array($k, ['z00', 'z01', 'z02', 'njb', 'tkb']), ARRAY_FILTER_USE_KEY);
+    $toCheck = $this->getLeastSignificantWrongBit($values, $expected);
+
+    // Find the gate that is 'wrong'.
+    $this->findMistake($gates['z' . str_pad($toCheck, 2, '0', STR_PAD_LEFT)], $toCheck);
+    return 0;
   }
 
-  protected function resolveKahns(array $gates, array $values) {
-    $valuePaths = [];
-    $indegrees = array_fill_keys(array_keys($gates), 0);
-    // Build a graph where we map all inputs to the operations they're part of.
-    $graph = [];
-    foreach ($gates as $output => $operation) {
-      $indegrees[$output] += 2;
-      list($in1, $in2) = $operation;
-      $graph[$in1] ??= [];
-      $graph[$in2] ??= [];
-      $graph[$in1][] = $operation;
-      $graph[$in2][] = $operation;
+  protected function isValid($gate, $bitNum) {
+    if ($bitNum === 0) {
+      $pair = [$gate[0], $gate[1]];
+      sort($pair);
+      $op = $gate[2];
+      return $pair === ['x00', 'y00'] && $op === 'XOR';
     }
 
-    $q = new \Ds\Queue();
-    foreach ($values as $gate => $value) {
-      $q->push($gate);
-      $valuePaths[$gate] = new \Ds\Set();
-    }
-
-    while(!$q->isEmpty()) {
-      $current = $q->pop();
-
-      // This gate as no gates that use its output as input.
-      if (!array_key_exists($current, $graph)) {
-        continue;
-      }
-
-      // Loop over all operations that use this (resolved) gate as input.
-      foreach ($graph[$current] as $operation) {
-        list ($in1, $in2, $operator, $output) = $operation;
-        $indegrees[$output]--;
-        // All the inputs of this gate are resolved, process it and add it to
-        // the queue.
-        if ($indegrees[$output] === 0) {
-          $valuePaths[$output] ??= $valuePaths[$current]->copy();
-          $pair = [$in1, $in2];
-          sort($pair);
-          $valuePaths[$output]->add(implode(' ', [...$pair, $operator, $output]));
-          switch ($operator) {
-            case 'AND':
-              $values[$output] = $values[$in1] & $values[$in2];
-              break;
-            case 'OR':
-              $values[$output] = $values[$in1] | $values[$in2];
-              break;
-
-            case 'XOR':
-              $values[$output] = $values[$in1] ^ $values[$in2];
-              break;
-          }
-          $q->push($output);
-        }
-      }
-    }
-
-    return [$values, $valuePaths];
+    // Uhm... verify the rest somehow. I need to figure this out still.
   }
 
   protected function getLeastSignificantWrongBit($values, $expected) {
